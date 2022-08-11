d36b7ea3e3662d827ff87b190550b731bbf67464
Valeria <mukhina_lera@list.ru>
2022-08-11 22:14:53 +0300

    .\010

---
+++ Task10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введи трёхзначное число: ");
+int threeDigitA = Convert.ToInt32(Console.ReadLine());
+string stringA = Convert.ToString(threeDigitA);
+Console.WriteLine("вторая цифра этого числа: "+stringA[1]);
