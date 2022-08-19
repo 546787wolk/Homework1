58c70f50a52b48d9d824b2d854875859b1fa9437
Valeria <mukhina_lera@list.ru>
2022-08-19 19:25:41 +0300

    .\Task029

---
+++ Task_29/Program.cs
@@ -0,0 +1,40 @@
+﻿//Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+class Program
+{
+    static void WriteArray(int[]array)
+ 
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]+ " ");
+        }
+    }
+
+static void Main(string [] args)
+{
+    string number = Console.ReadLine();
+    string [] array = new string [number.Length];
+int index = 0;
+foreach (var item in number)
+   {
+
+   if (item != ',')
+   {
+    array [index]+= item.ToString();
+   }else{
+index++;
+   }
+    
+  }
+  
+
+Console.Write ( "[ ");
+  
+for (int i = 0; i<=index-1; i++)
+{
+    Console.Write(array[i] + ",");
+}
+  Console.Write(array[index] + "]");
+
+}
+}
